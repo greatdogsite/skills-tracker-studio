6ec60ffc6b9b848083a564482dbea4008b2e06fa
Mango <greatdogsite@gmail.com>
2023-04-20 15:26:44 -0500

    initial

---
+++ src/test/java/com/mango/skillstrackerstudio/SkillsTrackerStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.skillstrackerstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsTrackerStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
